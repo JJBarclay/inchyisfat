03b00fbf8a07c640008c0584f7dd032056f5f692
Brandon Barclay <brandon.j.barclay@intel.com>
2017-11-16 21:00:31 -0800

    it works yay

--- m2.py
+++ m2.py
@@ -1,9 +1,7 @@
+#!/nfs/site/itools/em64t_SLES11/pkgs/python/3.4.1/bin/python3.4
 import turtle
 import math
 import random
-random.seed(0) # fix the guesses to make debug easier; remove in production.
-y_locations = {0:750,1:650,2:550,3:450,4:350,5:250,6:150,7:50} #global constant of y-locations of game board columns
-x_locations = {0:60,1:160,2:260,3:360,4:460,5:560,6:660,7:760} #global constant of x-locations of game board rows
 
 def square(size, pencolor, fillcolor):
     turtle.pendown
@@ -29,8 +27,8 @@ def make_board():
             turtle.forward(100)
         turtle.forward(10)
         turtle.color('black')
-        y_cor = turtle.ycor()
-        turtle.goto(10,y_cor + 100)
+        col_cor = turtle.ycor()
+        turtle.goto(10,col_cor + 100)
     turtle.forward(50)
     for i in range(2):
         for num in '01234567':
@@ -56,32 +54,44 @@ def board_start():
     place_token(3,4,'black')
     return(board_grid) #creates game board matrix #DO NOT FORGET TO MAKE GAME BOARD VARIABLE HERE TO PASS INTO THE FUNCTIONS
 
-def place_token(x_loc,y_loc,color):
-    turtle.goto((x_locations[x_loc]),(y_locations[y_loc]))
+def place_token(row_loc,col_loc,color):
+    print("place_token: "+str((row_loc,col_loc,color)))
+#    col_locations = {0:750,1:650,2:550,3:450,4:350,5:250,6:150,7:50} #global constant of y-locations of game board columns
+#    row_locations = {0:60,1:160,2:260,3:360,4:460,5:560,6:660,7:760} #global constant of x-locations of game board rows
+#    x = (col_locations[col_loc])
+#    y = (row_locations[row_loc])
+
+    x = col_loc * 100 + 55
+    y = (7 - row_loc) * 100 + 45
+
+    print("goto "+str((x,y)))
+
+    turtle.goto(x,y)
     turtle.shape('circle')
     turtle.shapesize(2.2,2.2)
     turtle.color(color)
     turtle.stamp() #uses global dictionaries to set down token of a player's say
+    turtle.update()
 
 def PC_turn(board):
     place = list(selectNextPlay(board))
     validity,to_flip = isValidMove(board, place[0],place[1], 'W')
     if validity == True:
-        x_loc = int(place[0])
-        y_loc = int(place[1])
-        place_token(x_loc, y_loc, 'white')
-        board[x_loc][y_loc] = 'W'
+        row_loc = int(place[0])
+        col_loc = int(place[1])
+        place_token(row_loc, col_loc, 'white')
+        board[row_loc][col_loc] = 'W'
         for place in to_flip.keys():
             if place != 'validity':
-                x_place = int(place[0])
-                y_place = int(place[1])
-                place_token(x_place, y_place, 'white')
-                board[x_place][y_place] = 'W'
+                row_place = int(place[0])
+                col_place = int(place[1])
+                place_token(row_place, col_place, 'white')
+                board[row_place][col_place] = 'W'
         return(board)
 
-def on_board(x_loc, y_loc):
-    if x_loc > -1 and x_loc < 8:
-        if y_loc > -1 and y_loc < 8:
+def on_board(row_loc, col_loc):
+    if row_loc > -1 and row_loc < 8:
+        if col_loc > -1 and col_loc < 8:
             return(True)
     else:
         return(False)
@@ -113,7 +123,7 @@ def neighbor_check(board,row,col,color): #true or false for if there is an oppon
         return(False)
 
 def youre_surrounded(board,row,col,color,wanted_neighbors):
-    lets_flip = {} #dictionary to fill with locations to flip with what color to flip to
+    to_flip = {} #dictionary to fill with locations to flip with what color to flip to
     if color == 'B': #get opposing color information
         opponent = 'W'
     else:
@@ -121,28 +131,48 @@ def youre_surrounded(board,row,col,color,wanted_neighbors):
 
     validity = False
     for location in wanted_neighbors.keys(): #for each location that has an opponent's token
+        print( "opponent="+opponent+" location="+str(location))
         for row_shift, col_shift in [[-1,-1],[0,-1],[1,-1],[-1,0],[1,0],[-1,1],[0,1],[1,1]]: #for directional checking
             temp_flip={}
             can_flip=True
+            print("Im at "+str((row,col))+" and looking at shifty neighbor "+str(location))
             if (row_shift + row,col_shift + col) == location: #find which shift for directional checking
+                print("Trying line in direction row_shift="+str(row_shift)+" col_shift="+str(col_shift))
                 for multiplier in [1,2,3,4,5,6,7]: #max distance is 7 from initial
-                    if on_board((multiplier*row_shift)+row,(multiplier*col_shift)+col):
-                        if board[(multiplier*row_shift)+row][(multiplier*col_shift)+col] == opponent:
-                            temp_flip[(multiplier*row_shift)+row,(multiplier*col_shift)+col] = color
-                        if board[(multiplier*row_shift)+row][(multiplier*col_shift)+col] == color:
-                            can_flip = False
-                            break
+                    looking_at_row = (multiplier*row_shift)+row
+                    looking_at_col = (multiplier*col_shift)+col
+                    print("  Looking at r="+str(looking_at_row)+" c="+str(looking_at_col))
+                    if not on_board(looking_at_row, looking_at_col): # went past the edge of the board
+                        can_flip = False
+                        print("    NOT ON BOARD. FAIL.")
+                        break
+                    piece_at_pos = board[looking_at_row][looking_at_col]
+                    if piece_at_pos == opponent:
+                        temp_flip[looking_at_row, looking_at_col] = color
+                        print("    can flip")
+                    elif piece_at_pos == color:
+                        print("    FOUND MY PAISANO.  YAY can_flip=1")
+                        can_flip = True
+                        break
+                    else: # we found an unoccupied square
+                        print("    HIT A BLANK. FAIL.")
+                        can_flip = False
+                        break
                 if can_flip:
                     validity=True
-                    lets_flip.update(temp_flip)
-    lets_flip['validity'] = validity
-    return(lets_flip)
+                    to_flip.update(temp_flip)
+    if validity:
+        to_flip[row,col] = color
+        print("we gonna flip "+str(to_flip))
+    to_flip['validity'] = validity
+    return(to_flip)
 
 def isValidMove(board, row, col, color): #checks if move is valid by calling helper functions
-    x_val = int(row)
-    y_val = int(col)
+
+    row_val = int(row)
+    col_val = int(col)
     neighbor_dict = neighborhood_watch(board, row, col)
-    if board[x_val][y_val] not in ['B','W']:
+    if board[row_val][col_val] not in ['B','W']:
         if neighbor_check(board, row, col,color) !=False:
             wanted_ones = neighbor_check(board,row,col,color)
             if youre_surrounded(board,row,col,color,wanted_ones) != {}:
@@ -153,37 +183,47 @@ def isValidMove(board, row, col, color): #checks if move is valid by calling hel
 
 def getValidMoves(board, color):
     valid_moves = []
-    for x in range(8):
-        for y in range(8):
-            validity,to_flip = isValidMove(board,x,y,color)
+    for row in range(8):
+        for col in range(8):
+            validity,to_flip = isValidMove(board,row,col,color)
             if validity == True:
-                valid_moves.append([x,y])
+                valid_moves.append([row,col])
     return(valid_moves)
 
 def human_turn(board):
     locs = list(turtle.textinput('Your Turn','Enter Row, Column Numbers'))
     if len(locs) != 3:
         locs = list(turtle.textinput('Your Turn','Enter Row, Column Numbers. Please ensure there is only a comma between numbers'))
-    #print("locs="+str(locs))
+    print("human locs entered="+str(locs))
     if len(locs) == 3:
         if locs == '':
             quit()
-        validity,to_flip = isValidMove(board, int(locs[0]),int(locs[2]),'B')
-        if validity == True:
-            x_loc = int(locs[0])
-            y_loc = int(locs[2])
-            place_token(x_loc, y_loc, 'black')
-            board[x_loc][y_loc] = 'B'
+        human_row = int(locs[0])
+        human_col = int(locs[2])
+        validity,to_flip = isValidMove(board, human_row, human_col, 'B')
+        if validity:
+            place_token(human_row, human_col, 'black')
+            board[human_row][human_col] = 'B'
+            print("to_flip="+str(to_flip))
+            print("i haz the keyz="+str(to_flip.keys()))
             for place in to_flip.keys():
-                print(place)
+                print("I AM AT place="+str(place))
+                if place == "validity":
+                    continue
+                print("I AM *still* AT place="+str(place))
+                print(str(place))
                 print(place[0])
                 print(place[1])
-                if place != 'validity':
-                    x_place = int(place[0])
-                    y_place = int(place[1])
-                    place_token(x_place, y_place, 'black')
-                    board[x_place][y_place] = 'B'
-                return(board)
+                #if place != 'validity':
+                row_place = int(place[0])
+                col_place = int(place[1])
+                place_token(row_place, col_place, 'black')
+                board[row_place][col_place] = 'B'
+                print("place token B at r="+str(row_place)+" c="+str(col_place))
+
+            print("FLIPPING DONE")
+            return(board)
+
     if locs == []:
         quit()
     else:

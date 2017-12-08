817544c658680c170bc37ada032d74c5c9eed1a8
JJBarclay <33739531+JJBarclay@users.noreply.github.com>
2017-12-08 13:18:53 -0600

    Added Test

--- exam3.revision/fractal.py
+++ exam3.revision/fractal.py
@@ -12,8 +12,8 @@ class Display:
          self.__s.screensize(200,200)
          self.t.speed(0)
          self.__s.tracer(100,0)
-         for y in range(-200, -150):
-            for x in range(-200, -150):
+         for y in range(-200, 201):
+            for x in range(-200, 201):
                 self.t.goto(x,y)
                 x = x/100
                 y = y/100
@@ -27,3 +27,14 @@ class Display:
                 self.t.forward(1)
                 self.t.penup()
          #self.__screen.onlick(self.__mouseEvent)
+
+def test():
+    turtle.tracer(1000, 0)
+    for x in range(-200, 200):
+        for y in range(-200, 200):
+            turtle.penup()
+            turtle.goto(x,y)
+            turtle.pencolor('blue')
+            turtle.pendown()
+            turtle.forward(.5)
+            turtle.penup()
